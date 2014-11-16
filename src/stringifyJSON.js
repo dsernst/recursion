a3932c848763226c0f9811fa60fe9d682412110a
dsernst <git@dsernst.com>
2014-11-16 13:52:40 -0600

    pass tests for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,68 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function (obj) {
+  var str;
+  var i;
+
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (typeof obj === 'boolean') {
+    if (obj) {
+      return 'true';
+    }
+    return 'false';
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    str = '[';
+
+    for (i = 0; i < obj.length; i += 1) {
+      if (i > 0) {
+        str += ',';
+      }
+      str += stringifyJSON(obj[i]);
+    }
+    str += ']';
+
+    return str;
+  }
+
+  if (typeof obj === 'object') {
+    str = '{';
+
+    for (i in obj) {
+      if (obj.hasOwnProperty(i)) {
+        if (typeof i !== 'function' && typeof obj[i] !== 'function') {
+          if (i !== 'undefined' && obj[i] !== 'undefined') {
+            if (str.length > 1) {
+              str += ',';
+            }
+            str += stringifyJSON(i);
+            str += ':';
+            str += stringifyJSON(obj[i]);
+          }
+        }
+      }
+    }
+
+    str += '}';
+
+    return str;
+  }
+
+  if (typeof obj === 'function') {
+    return;
+  }
+
 };
